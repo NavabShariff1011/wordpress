f40ad124f0fce7931a72198bd662485f5ed3c883
root <root@ip-172-31-44-29.ap-south-1.compute.internal>
2022-10-16 03:26:24 +0000

    hhh

--- dbconfigure.sh
+++ dbconfigure.sh
@@ -20,4 +20,31 @@ sudo mysql -e "GRANT ALL PRIVILEGES ON $dbname. * TO '$username'@'localhost' WIT
 
 sudo mysql -e "FLUSH PRIVILEGES;"
 
+cat <<EOF > /var/www/html/wordpress/wp-config.php
+<?php
+define( 'DB_NAME', '$dbname' );
 
+/** Database username */
+define( 'DB_USER', '$username' );
+
+/** Database password */
+define( 'DB_PASSWORD', '$passwd' );
+
+/** Database hostname */
+define( 'DB_HOST', 'localhost' );
+
+define( 'DB_CHARSET', 'utf8' );
+
+/** The database collate type. Don't change this if in doubt. */
+define( 'DB_COLLATE', '' );
+
+$table_prefix = 'wp_';
+define( 'WP_DEBUG', false );
+
+if ( ! defined( 'ABSPATH' ) ) {
+        define( 'ABSPATH', __DIR__ . '/' );
+}
+
+/** Sets up WordPress vars and included files. */
+require_once ABSPATH . 'wp-settings.php';
+EOF
